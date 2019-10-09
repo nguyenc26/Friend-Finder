611cd1854f752b2078e3de3abe3df9853e7d00cf
Cuong XPS <nguyenc72694@gmail.com>
2019-10-08 19:24:30 -0500

    adding files

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friends = [
+    {
+        "name": "Tobi",
+        "photo": "https://vignette.wikia.nocookie.net/villains/images/0/01/TobiOrangeMaskRender.png/revision/latest?cb=20180801075633",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Itachi",
+        "photo": "https://vignette.wikia.nocookie.net/villains/images/a/a4/Itachi_Uchiha_%28Naruto%29.png/revision/latest/scale-to-width-down/487?cb=20120530222228",
+        "scores": [
+            5,
+            4,
+            3,
+            5,
+            3,
+            5,
+            1,
+            2,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "Kisame",
+        "photo": "https://vignette.wikia.nocookie.net/villains/images/2/25/Kisame.png/revision/latest/scale-to-width-down/583?cb=20190112142144",
+        "scores": [
+            1,
+            3,
+            5,
+            1,
+            3,
+            5,
+            1,
+            1,
+            5,
+            3
+        ]
+    },
+    {
+        "name": "Sasori",
+        "photo": "https://vignette.wikia.nocookie.net/villains/images/d/d0/Sasori_of_the_red_sand.png/revision/latest/scale-to-width-down/620?cb=20180817181455",
+        "scores": [
+            3,
+            4,
+            4,
+            1,
+            4,
+            4,
+            3,
+            3,
+            5,
+            4
+        ]
+    },
+    {
+        "name": "Deidara",
+        "photo": "https://vignette.wikia.nocookie.net/villains/images/3/3c/Deidara02.png/revision/latest/scale-to-width-down/620?cb=20180817185320",
+        "scores": [
+            5,
+            4,
+            2,
+            1,
+            3,
+            5,
+            4,
+            2,
+            2,
+            2
+        ]
+    },
+    {
+        "name": "Kakuzu",
+        "photo": "https://vignette.wikia.nocookie.net/villains/images/a/a1/KakuzuRender.png/revision/latest/scale-to-width-down/409?cb=20180818072314",
+        "scores": [
+            3,
+            4,
+            3,
+            4,
+            3,
+            2,
+            4,
+            3,
+            3,
+            4
+        ]
+    },
+    {
+        "name": "Hidan",
+        "photo": "https://vignette.wikia.nocookie.net/villains/images/7/79/Hidan_full.png/revision/latest/scale-to-width-down/619?cb=20141002022038",
+        "scores": [
+            3,
+            4,
+            3,
+            3,
+            2,
+            3,
+            3,
+            2,
+            4,
+            5
+        ]
+    },
+    {
+        "name": "Konan",
+        "photo": "https://vignette.wikia.nocookie.net/villains/images/5/57/Konan.png/revision/latest/scale-to-width-down/620?cb=20190112141901",
+        "scores": [
+            5,
+            4,
+            2,
+            1,
+            3,
+            2,
+            3,
+            2,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "Pain",
+        "photo": "https://vignette.wikia.nocookie.net/villains/images/9/96/Deva_Parth_%28Naruto%29.png/revision/latest?cb=20120817200727",
+        "scores": [
+            5,
+            2,
+            2,
+            2,
+            2,
+            2,
+            3,
+            2,
+            4,
+            2
+        ]
+    },
+    {
+        "name": "Zetsu",
+        "photo": "https://vignette.wikia.nocookie.net/villains/images/d/dc/BlackZetsuFull.png/revision/latest/scale-to-width-down/503?cb=20190722224033",
+        "scores": [
+            4,
+            3,
+            4,
+            1,
+            5,
+            2,
+            5,
+            3,
+            1,
+            4
+        ]
+    },
+];
+
+module.exports = friends;
